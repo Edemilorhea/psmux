88a81610a4efd37b23aed22a64d3cad90bd27dda
TC Tseng <tc_tseng@gss.com.tw>
2026-06-08 17:12:11 +0800

    fix: add force signal mode for send-keys Ctrl-C

--- src/types.rs
+++ src/types.rs
@@ -1320,7 +1320,7 @@ pub enum CtrlReq {
     ToggleSync,
     SetPaneTitle(String),
     SetPaneStyle(String),
-    SendKeys(String, bool),
+    SendKeys(String, bool, bool),  // (keys, literal, force_signal)
     SendKeysX(String),  // send-keys -X copy-mode-command
     SelectPane(String, bool),
     SelectWindow(usize),
